03b6bd744038450a2503e7994ec7ce6c45f95d65
planetageek <planetageekmx@gmail.com>
2014-07-17 00:54:00 -0500

    Create JavaArduino.java

---
+++ JavaArduino.java
@@ -0,0 +1,20 @@
+/*
+ *  Planeta Geek
+ *  17/O7/2014
+ *
+ */
+package javaarduino;
+
+/**
+ *
+ * @author edkepex
+ */
+public class JavaArduino {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
